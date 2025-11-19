932715413160b78b21ab9efe1fd7bc27ea004a44
VivekBansalBoxinall <vivekbansal@boxinallsoftech.com>
2025-11-19 16:39:56 +0530

    Add field standardization and cleanse previewer to agents import in routes

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,5 +1,5 @@
 # Agents package
-from . import unified_profiler, drift_detector, score_risk, readiness_rater, governance_checker, test_coverage_agent, null_handler, outlier_remover, type_fixer, duplicate_resolver, quarantine_agent, cleanse_writeback
+from . import unified_profiler, drift_detector, score_risk, readiness_rater, governance_checker, test_coverage_agent, null_handler, outlier_remover, type_fixer, duplicate_resolver, quarantine_agent, cleanse_writeback, field_standardization, cleanse_previewer
 
 __all__ = [
     'unified_profiler',
@@ -13,5 +13,7 @@ __all__ = [
     'type_fixer',
     'duplicate_resolver',
     'quarantine_agent',
-    'cleanse_writeback'
+    'cleanse_writeback',
+    'field_standardization',
+    'cleanse_previewer'
 ]
